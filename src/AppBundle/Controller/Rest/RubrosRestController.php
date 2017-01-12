a7ce44acfc68a80ae81bff60438e06d718ca7629
matudelatower <matias.delatorre89@gmail.com>
2017-01-12 12:07:55 -0300

    Se corrige servicio SubRubros

--- src/AppBundle/Controller/Rest/RubrosRestController.php
+++ src/AppBundle/Controller/Rest/RubrosRestController.php
@@ -11,22 +11,54 @@ class RubrosRestController extends FOSRestController {
 
 		$rubros = $this->getDoctrine()->getRepository( "AppBundle:Rubro" )->findAll();
 
-	
+
 		$vista = $this->view( $rubros,
-			200 )
-		;
+			200 );
 
 		return $this->handleView( $vista );
 	}
 
 	public function getRubrosEmpresaAction( Request $request, $empresaId ) {
 
-		$rubros = $this->getDoctrine()->getRepository( "AppBundle:Categoria" )->findAllByEmpresa($empresaId);
+		$rubros = $this->getDoctrine()->getRepository( "AppBundle:Categoria" )->findAllByEmpresa( $empresaId );
 
 
 		$vista = $this->view( $rubros,
-			200 )
-		;
+			200 );
+
+		return $this->handleView( $vista );
+	}
+
+	public function getRubrosSlugAction( Request $request, $slug ) {
+
+		$criteria = array(
+			'activo' => true,
+			'slug'   => $slug
+		);
+
+		$rubros = $this->getDoctrine()->getRepository( "AppBundle:Rubro" )->findBy( $criteria );
+
+
+		$vista = $this->view( $rubros,
+			200 );
+
+		return $this->handleView( $vista );
+	}
+
+	public function getSubrubrosSlugrubroAction( Request $request, $slug ) {
+
+		$criteria = array(
+			'activo' => true,
+			'slug'   => $slug
+		);
+
+		$rubro = $this->getDoctrine()->getRepository( "AppBundle:Rubro" )->findBy( $criteria );
+
+		$subRubros = $this->getDoctrine()->getRepository( "AppBundle:SubRubro" )->findByRubro( $rubro );
+
+
+		$vista = $this->view( $subRubros,
+			200 );
 
 		return $this->handleView( $vista );
 	}
