76d5788b481ca6c69dd6deab32c6c132f44d8e5f
matudelatower <matias.delatorre89@gmail.com>
2017-01-13 12:04:00 -0300

    Se corrigen datos de direccion en el service

--- src/AppBundle/Entity/Direccion.php
+++ src/AppBundle/Entity/Direccion.php
@@ -86,7 +86,7 @@ class Direccion extends BaseClass {
 
 	/**
 	 * @VirtualProperty()
-	 * @SerializedName("localidad")
+	 * @SerializedName("json_localidad")
 	 */
 	public function getJsonLocalidad() {
 
@@ -102,7 +102,7 @@ class Direccion extends BaseClass {
 	}
 	/**
 	 * @VirtualProperty()
-	 * @SerializedName("departamento")
+	 * @SerializedName("json_departamento")
 	 */
 	public function getJsonDepartamento() {
 
@@ -118,7 +118,7 @@ class Direccion extends BaseClass {
 	}
 	/**
 	 * @VirtualProperty()
-	 * @SerializedName("provincia")
+	 * @SerializedName("json_provincia")
 	 */
 	public function getJsonProvincia() {
 
@@ -134,7 +134,7 @@ class Direccion extends BaseClass {
 	}
 	/**
 	 * @VirtualProperty()
-	 * @SerializedName("pais")
+	 * @SerializedName("json_pais")
 	 */
 	public function getJsonPais() {
 
